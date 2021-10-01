b917fa90d3e0ac5349edf404a54516b1a9da0c48
Inpanic4 <sargyropoulos4@gmail.com>
2021-10-01 18:08:10 +0300

    changed to factory seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,84 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // If we run seed again this means drop the existing 
-        // tables and create new ones truncate();
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        // make 2 users
-        $user = User::factory(2)->create();
-
-        // make a category in my database
-       $personal= Category::create([
-            'name' => 'Personal',
-            'slug' =>'personal',
+       $user= User::factory()->create([
+            'name'=>'John Doe'
         ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' =>'family',
-        ]);
-
-       $work= Category::create([
-            'name' => 'Work',
-            'slug' =>'work',
-        ]);
-
-        Post::create([
-            'user_id' => $user->first()->id,
-            'category_id' =>$personal->first()->id,
-            'title'=>'My First Post',
-            'slug' => 'my-first-post',
-            'excerpt' => 'Excerpt first post',
-            'body'=> 'Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit, sed do eiusmod tempor 
-            incididunt ut labore et dolore magna aliqua.
-             Consectetur libero id faucibus nisl. Dolor sed 
-             viverra ipsum nunc aliquet bibendum. Diam quam nulla 
-             porttitor massa id neque aliquam. In nisl nisi 
-             scelerisque eu ultrices vitae. Aenean euismod 
-             elementum nisi quis eleifend quam. Suspendisse 
-             interdum consectetur libero id faucibus. In hac 
-             habitasse platea dictumst quisque sagittis. Velit 
-             euismod in pellentesque massa placerat duis ultricies lacus.'
-        ]);
-        Post::create([
-            'user_id' => $user->first()->id,
-            'category_id' =>$family->id,
-            'title'=>'My Second Post',
-            'slug' => 'my-second-post',
-            'excerpt' => 'Excerpt first post',
-            'body'=> 'Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit, sed do eiusmod tempor 
-            incididunt ut labore et dolore magna aliqua.
-             Consectetur libero id faucibus nisl. Dolor sed 
-             viverra ipsum nunc aliquet bibendum. Diam quam nulla 
-             porttitor massa id neque aliquam. In nisl nisi 
-             scelerisque eu ultrices vitae. Aenean euismod 
-             elementum nisi quis eleifend quam. Suspendisse 
-             interdum consectetur libero id faucibus. In hac 
-             habitasse platea dictumst quisque sagittis. Velit 
-             euismod in pellentesque massa placerat duis ultricies lacus.'
-        ]);
-        Post::create([
-            'user_id' => $user->first()->id,
-            'category_id' =>$work->id,
-            'title'=>'My Third Post',
-            'slug' => 'my-third-post',
-            'excerpt' => 'Excerpt first post',
-            'body'=> 'Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit, sed do eiusmod tempor 
-            incididunt ut labore et dolore magna aliqua.
-             Consectetur libero id faucibus nisl. Dolor sed 
-             viverra ipsum nunc aliquet bibendum. Diam quam nulla 
-             porttitor massa id neque aliquam. In nisl nisi 
-             scelerisque eu ultrices vitae. Aenean euismod 
-             elementum nisi quis eleifend quam. Suspendisse 
-             interdum consectetur libero id faucibus. In hac 
-             habitasse platea dictumst quisque sagittis. Velit 
-             euismod in pellentesque massa placerat duis ultricies lacus.'
+        Post::factory(5)->create([
+            'user_id' =>$user->id
         ]);
     }
 }
